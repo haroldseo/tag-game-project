0ccfe0b83f3759fd01dc504986ee696558878d56
Harold Seo <harold.seo.612@gmail.com>
2017-09-05 13:18:21 -0700

    added two players

--- js/main.js
+++ js/main.js
@@ -1,1 +1,13 @@
-console.log("hi")
+var player1 = $("#player1")
+var player2 = $("#player2")
+
+player1.css({
+    left: 10,
+    top: 310
+})
+player2.css({
+    left: 910,
+    top: 310
+})
+
+$("#playingField").append(player1, player2)
