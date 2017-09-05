d9ce8fd5aaebebe30d8ca501a693b9564b7aef42
Harold Seo <harold.seo.612@gmail.com>
2017-09-05 15:28:05 -0700

    can move player 2

--- js/main.js
+++ js/main.js
@@ -16,13 +16,26 @@ $("#playingField").append($player1, $player2)
 var $p1Move = $player1
 $(document).keydown(function(e) {
     if(e.keyCode === 37) {
-        $p1Move.css("left", ($p1Move.position().left - 10) + "px");
+        $p1Move.css("left", ($p1Move.position().left - 30) + "px");
     } else if (e.keyCode === 39) {
-        $p1Move.css("left", ($p1Move.position().left + 10) + "px");
+        $p1Move.css("left", ($p1Move.position().left + 30) + "px");
     } else if (e.keyCode === 38) {
-        $p1Move.css("top", ($p1Move.position().top - 10) + "px");
+        $p1Move.css("top", ($p1Move.position().top - 30) + "px");
     } else if (e.keyCode === 40) {
-        $p1Move.css("top", ($p1Move.position().top + 10) + "px");
+        $p1Move.css("top", ($p1Move.position().top + 30) + "px");
+    }
+})
+
+var $p2Move = $player2
+$(document).keydown(function(e) {
+    if(e.keyCode === 65) {
+        $p2Move.css("left", ($p2Move.position().left - 30) + "px");
+    } else if (e.keyCode === 68) {
+        $p2Move.css("left", ($p2Move.position().left + 30) + "px");
+    } else if (e.keyCode === 87) {
+        $p2Move.css("top", ($p2Move.position().top - 30) + "px");
+    } else if (e.keyCode === 83) {
+        $p2Move.css("top", ($p2Move.position().top + 30) + "px");
     }
 })
 
