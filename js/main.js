e61c1e3ca065d9f66d48c75962a378fbb845f1e3
Harold Seo <harold.seo.612@gmail.com>
2017-09-05 15:31:53 -0700

    switched movement keys for players

--- js/main.js
+++ js/main.js
@@ -15,26 +15,26 @@ $("#playingField").append($player1, $player2)
 //--------------------------
 var $p1Move = $player1
 $(document).keydown(function(e) {
-    if(e.keyCode === 37) {
+    if(e.keyCode === 65) {
         $p1Move.css("left", ($p1Move.position().left - 30) + "px");
-    } else if (e.keyCode === 39) {
+    } else if (e.keyCode === 68) {
         $p1Move.css("left", ($p1Move.position().left + 30) + "px");
-    } else if (e.keyCode === 38) {
+    } else if (e.keyCode === 87) {
         $p1Move.css("top", ($p1Move.position().top - 30) + "px");
-    } else if (e.keyCode === 40) {
+    } else if (e.keyCode === 83) {
         $p1Move.css("top", ($p1Move.position().top + 30) + "px");
     }
 })
 
 var $p2Move = $player2
 $(document).keydown(function(e) {
-    if(e.keyCode === 65) {
+    if(e.keyCode === 37) {
         $p2Move.css("left", ($p2Move.position().left - 30) + "px");
-    } else if (e.keyCode === 68) {
+    } else if (e.keyCode === 39) {
         $p2Move.css("left", ($p2Move.position().left + 30) + "px");
-    } else if (e.keyCode === 87) {
+    } else if (e.keyCode === 38) {
         $p2Move.css("top", ($p2Move.position().top - 30) + "px");
-    } else if (e.keyCode === 83) {
+    } else if (e.keyCode === 40) {
         $p2Move.css("top", ($p2Move.position().top + 30) + "px");
     }
 })
